0b5f47ce684c5cccf9fea8e1b9bb07ab04900425
Dellamora <francielle@dellamora.dev>
2023-11-15 09:35:41 -0300

    feat: add tailwind CSS

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Meteor } from "meteor/meteor";
+import { App } from "/imports/ui/App";
+import "./main.css";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container);
   root.render(<App />);
 });
